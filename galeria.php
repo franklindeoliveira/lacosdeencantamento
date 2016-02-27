7587b838b68792e519b9956dda91a4c49a3e535e
franklindeoliveira <franklindeoliveira@gmail.com>
2016-02-27 12:54:26 -0300

    galeria de fotos

--- galeria.php
+++ galeria.php
@@ -34,7 +34,8 @@
            lightbox.option({
                'resizeDuration': 200,
                'wrapAround': true,
-               'albumLabel': 'Imagem %1 de %2'
+               'albumLabel': 'Imagem %1 de %2',
+               'alwaysShowNavOnTouchDevices': true
            });
        });
     </script>
