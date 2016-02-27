1690cc4ad631db4b362aeb8e4d42cde6b3965d25
franklindeoliveira <franklindeoliveira@gmail.com>
2016-02-27 00:23:05 -0300

    atualizando lista de redes sociais

--- rodape.php
+++ rodape.php
@@ -3,9 +3,11 @@
         <img class="logoFooter" src="img/logo.jpg" alt="Logo Laços de Encantamento">
         <ul class="social">
             <li><a target="_blank" class="youtube" href="https://www.youtube.com/channel/UCiVmKurWXuDKy1uQvZFI2Lg">YouTube</a></li>
-            <li><a target="_blank" class="facebook" href="http://facebook.com/lacosdeencantamento">Facebook</a></li>
+            <li><a target="_blank" class="facebook" href="https://www.facebook.com/brincadeirasdebebelacos">Facebook</a></li>
+<!--
             <li><a target="_blank" class="twitter" href="http://twitter.com/lacosdeencantamento">Twitter</a></li>
             <li><a target="_blank" class="googleplus" href="http://plus.google.com/lacosdeencantamento">Google+</a></li>
+-->
         </ul>
     </div>
 </footer>
