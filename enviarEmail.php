14ec4d8fe2bb5c3bd105117cbfd083ff5838bd7d
franklindeoliveira <franklindeoliveira@gmail.com>
2016-02-26 23:08:42 -0300

    correcao envio de email

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,24 +1,21 @@
 <?php
-  //1 – Definimos Para quem vai ser enviado o email
-  $para = "franklindeoliveira@gmail.com";
-  //2 - resgatar o nome digitado no formulário e  grava na variavel $nome
-  $nome = $_POST['nome'];
-  // 3 - resgatar o assunto digitado no formulário e  grava na variavel //$assunto
-  $assunto = $_POST['assunto'];
-   //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
-  $mensagem = "<strong>Seu nome:  </strong>".$nome;
-  $mensagem .= "<br>  <strong>Nome do bebê: </strong>".$_POST['mensagem'];
-  $mensagem .= "<br>  <strong>Data de nascimento do bebê: </strong>".$_POST['mensagem'];
-  $mensagem .= "<br>  <strong>Seu email: </strong>".$_POST['mensagem'];
 
-//5 – agora inserimos as codificações corretas e  tudo mais.
+  $para = "franklindeoliveira@gmail.com";
+  $seuNome = $_POST['seuNome'];
+  $assunto = $_POST['assunto'];
+  $mensagem = "<strong>Seu nome:  </strong>".$nome;
+  $mensagem .= "<br>  <strong>Nome do bebê: </strong>";
+  $mensagem .= "<br>  <strong>Data de nascimento do bebê: </strong>";
+  $mensagem .= "<br>  <strong>Seu email: </strong>";
+
   $headers =  "Content-Type:text/html; charset=UTF-8\n";
-  $headers .= "From:  lacosdeencantamento.com.br<sistema@dominio.com.br>\n"; //Vai ser //mostrado que  o email partiu deste email e seguido do nome
-  $headers .= "X-Sender:  <sistema@dominio.com.br>\n"; //email do servidor //que enviou
+  $headers .= "From:  lacosdeencantamento.com.br<sistema@dominio.com.br>\n";
+  $headers .= "X-Sender:  <sistema@dominio.com.br>\n";
   $headers .= "X-Mailer: PHP  v".phpversion()."\n";
   $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-  $headers .= "Return-Path:  <sistema@dominio.com.br>\n"; //caso a msg //seja respondida vai para  este email.
+  $headers .= "Return-Path:  <sistema@dominio.com.br>\n";
   $headers .= "MIME-Version: 1.0\n";
 
-  mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
+  mail($para, $assunto, $mensagem, $headers);
+
 ?>
