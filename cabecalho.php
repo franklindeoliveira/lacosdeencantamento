cb3a0a6a7c007a4742facaa90206c91193c07610
franklindeoliveira <franklindeoliveira@gmail.com>
2016-03-03 03:10:04 -0300

    menu: cores, espacamentos, depoimentos

--- cabecalho.php
+++ cabecalho.php
@@ -2,12 +2,12 @@
     <h1><img class="logo logoHeader" src="img/logo.png" alt="Laços de Encantamento - Brincadeiras de Bebê"></h1>
     <nav class="menu-opcoes">
         <ul>
-            <li><a href="index.php"><button>Início</button></a></li>
-            <li><a href="sobre.php"><button>Sobre</button></a></li>
-            <li><a href="brincadeiras.php"><button>Brincadeiras</button></a></li>
-            <li><a href="contato.php"><button>Contato</button></a></li>
-            <li><a href="galeria.php"><button>Galeria de Fotos</button></a></li>
-            <li><a href="depoimentos.php"><button>Depoimentos</button></a></li>
+            <li><a href="index.php"><button class="amarelo">Início</button></a></li>
+            <li><a href="sobre.php"><button class="verde">Sobre</button></a></li>
+            <li><a href="brincadeiras.php"><button class="roxo">Brincadeiras</button></a></li>
+            <li><a href="contato.php"><button class="laranja">Contato</button></a></li>
+            <li><a href="galeria.php"><button class="amarelo">Galeria de Fotos</button></a></li>
+            <li><a href="depoimentos.php"><button class="verde">Depoimentos</button></a></li>
         </ul>
     </nav>
 </header>
--- depoimentos.php
+++ depoimentos.php
@@ -10,8 +10,22 @@
 <body>
     <?php include("cabecalho.php"); ?>
     <main>
-        <section class="depoimentos">
+        <section class="depoimentos contato">
             <h2>Depoimentos</h2>
+            
+            <div class="lista-email">
+                <h3 class="transparente">Participou da oficinas? Conte-nos o que achou!</h3>
+                <form action="contatoEmail.php" method="post">
+                    <input name="assunto" type="hidden" id="assunto" value="Contato"><br />
+                    <input name="nome" placeholder="Nome" type="text"><br />
+                    <input name="email" placeholder="Email" type="email"><br />
+                    <input name="assuntoUsuario" placeholder="Assunto" type="text"><br />
+                    <textarea name="mensagemUsuario" placeholder="Mensagem"></textarea>
+                    
+                    <input type="submit" value="Enviar!">
+                </form>
+            </div>
+<!--
             <blockcote>
                 <p>Descrição do depoimento... decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição</p>
                 <cite>Nome da citação</cite>
@@ -24,6 +38,7 @@
                 <p>Descrição do depoimento... decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição decrição</p>
                 <cite>Nome da citação</cite>
             </blockcote>
+-->
         </section>
     </main>
     <?php include("rodape.php"); ?>
