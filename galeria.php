c43dc1437638d313f75554447bd0d038ab30389a
franklindeoliveira <franklindeoliveira@gmail.com>
2016-03-20 18:42:37 -0300

    otimizando carregamento das imagens 5

--- galeria.php
+++ galeria.php
@@ -1,39 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Laços de Encantamento: Galeria de Fotos</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/estilos.css">
-	<link rel="stylesheet" href="lib/css/lightbox.css">
-	<link href='https://fonts.googleapis.com/css?family=Amatic+SC:400,700' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="lib/css/lightbox.css">
+    <link href='https://fonts.googleapis.com/css?family=Amatic+SC:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans:300' rel='stylesheet' type='text/css'>
 </head>
+
 <body>
     <?php include("cabecalho.php"); ?>
-    <main>
-        <section class="galeria-fotos">
-            <h2>Galeria de fotos</h2>
-<!--            <a href="img/carrossel1.jpg" data-lightbox="roadtrip" data-title="Nossa que imagem linda!">-->
-            
-        </section>
-    </main>
-    <?php include("rodape.php"); ?>
-    <script src="js/jquery-1.12.0.min.js"></script>
-    <script src="lib/js/lightbox.js"></script>
-    
-    <script src="js/galeria.js"></script>
-    
-    <script>
-       $(function () {
-           lightbox.option({
-               'resizeDuration': 200,
-               'wrapAround': true,
-               'albumLabel': 'Imagem %1 de %2',
-               'alwaysShowNavOnTouchDevices': true
-           });
-       });
-    </script>
+        <main>
+            <section class="galeria-fotos">
+                <h2>Galeria de fotos</h2>
+                <!-- <a href="img/carrossel1.jpg" data-lightbox="roadtrip" data-title="Nossa que imagem linda!"> -->
+
+                <a href="img/galeria/originais/foto1.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto1.jpg" alt="Foto 1"></a>
+                <a href="img/galeria/originais/foto2.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto2.jpg" alt="Foto 2"></a>
+                <a href="img/galeria/originais/foto3.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto3.jpg" alt="Foto 3"></a>
+                <a href="img/galeria/originais/foto4.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto4.jpg" alt="Foto 4"></a>
+                <a href="img/galeria/originais/foto5.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto5.jpg" alt="Foto 5"></a>
+                <a href="img/galeria/originais/foto6.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto6.jpg" alt="Foto 6"></a>
+                <a href="img/galeria/originais/foto7.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto7.jpg" alt="Foto 7"></a>
+                <a href="img/galeria/originais/foto8.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto8.jpg" alt="Foto 8"></a>
+                <a href="img/galeria/originais/foto9.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto9.jpg" alt="Foto 9"></a>
+                <a href="img/galeria/originais/foto10.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto10.jpg" alt="Foto 10"></a>
+                <a href="img/galeria/originais/foto11.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto11.jpg" alt="Foto 11"></a>
+                <a href="img/galeria/originais/foto12.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto12.jpg" alt="Foto 12"></a>
+                <a href="img/galeria/originais/foto13.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto13.jpg" alt="Foto 13"></a>
+                <a href="img/galeria/originais/foto14.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto14.jpg" alt="Foto 14"></a>
+                <a href="img/galeria/originais/foto15.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto15.jpg" alt="Foto 15"></a>
+                <a href="img/galeria/originais/foto16.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto16.jpg" alt="Foto 16"></a>
+                <a href="img/galeria/originais/foto17.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto17.jpg" alt="Foto 17"></a>
+                <a href="img/galeria/originais/foto18.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto18.jpg" alt="Foto 18"></a>
+                <a href="img/galeria/originais/foto19.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto19.jpg" alt="Foto 19"></a>
+                <a href="img/galeria/originais/foto20.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto20.jpg" alt="Foto 20"></a>
+                <a href="img/galeria/originais/foto21.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto21.jpg" alt="Foto 21"></a>
+                <a href="img/galeria/originais/foto22.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto22.jpg" alt="Foto 22"></a>
+                <a href="img/galeria/originais/foto23.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto23.jpg" alt="Foto 23"></a>
+                <a href="img/galeria/originais/foto24.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto24.jpg" alt="Foto 24"></a>
+                <a href="img/galeria/originais/foto25.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto25.jpg" alt="Foto 25"></a>
+                <a href="img/galeria/originais/foto26.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto26.jpg" alt="Foto 26"></a>
+                <a href="img/galeria/originais/foto27.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto27.jpg" alt="Foto 27"></a>
+                <a href="img/galeria/originais/foto28.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto28.jpg" alt="Foto 28"></a>
+                <a href="img/galeria/originais/foto29.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto29.jpg" alt="Foto 29"></a>
+                <a href="img/galeria/originais/foto30.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto30.jpg" alt="Foto 30"></a>
+                <a href="img/galeria/originais/foto31.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto31.jpg" alt="Foto 31"></a>
+                <a href="img/galeria/originais/foto32.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto32.jpg" alt="Foto 32"></a>
+                <a href="img/galeria/originais/foto33.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto33.jpg" alt="Foto 33"></a>
+                <a href="img/galeria/originais/foto34.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto34.jpg" alt="Foto 34"></a>
+                <a href="img/galeria/originais/foto35.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto35.jpg" alt="Foto 35"></a>
+                <a href="img/galeria/originais/foto36.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto36.jpg" alt="Foto 36"></a>
+                <a href="img/galeria/originais/foto37.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto37.jpg" alt="Foto 37"></a>
+                <a href="img/galeria/originais/foto38.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto38.jpg" alt="Foto 38"></a>
+                <a href="img/galeria/originais/foto39.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto39.jpg" alt="Foto 39"></a>
+                <a href="img/galeria/originais/foto40.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto40.jpg" alt="Foto 40"></a>
+                <a href="img/galeria/originais/foto41.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto41.jpg" alt="Foto 41"></a>
+                <a href="img/galeria/originais/foto42.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto42.jpg" alt="Foto 42"></a>
+                <a href="img/galeria/originais/foto43.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto43.jpg" alt="Foto 43"></a>
+                <a href="img/galeria/originais/foto44.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto44.jpg" alt="Foto 44"></a>
+                <a href="img/galeria/originais/foto45.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto45.jpg" alt="Foto 45"></a>
+                <a href="img/galeria/originais/foto46.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto46.jpg" alt="Foto 46"></a>
+                <a href="img/galeria/originais/foto47.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto47.jpg" alt="Foto 47"></a>
+                <a href="img/galeria/originais/foto48.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto48.jpg" alt="Foto 48"></a>
+                <a href="img/galeria/originais/foto49.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto49.jpg" alt="Foto 49"></a>
+                <a href="img/galeria/originais/foto50.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto50.jpg" alt="Foto 50"></a>
+                <a href="img/galeria/originais/foto51.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto51.jpg" alt="Foto 51"></a>
+                <a href="img/galeria/originais/foto52.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto52.jpg" alt="Foto 52"></a>
+                <a href="img/galeria/originais/foto53.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto53.jpg" alt="Foto 53"></a>
+                <a href="img/galeria/originais/foto54.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto54.jpg" alt="Foto 54"></a>
+
+            </section>
+        </main>
+        <?php include("rodape.php"); ?>
+        <script src="js/jquery-1.12.0.min.js"></script>
+        <script src="lib/js/lightbox.js"></script>
+
+        <script src="js/galeria.js"></script>
+
+        <script>
+            $(function () {
+                lightbox.option({
+                    'resizeDuration': 200,
+                    'wrapAround': true,
+                    'albumLabel': 'Imagem %1 de %2',
+                    'alwaysShowNavOnTouchDevices': true
+                });
+            });
+        </script>
 </body>
+
 </html>
