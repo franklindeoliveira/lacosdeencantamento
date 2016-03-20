b3cce2769eff3e3aff6bd2c36231aeaa79f77536
franklindeoliveira <franklindeoliveira@gmail.com>
2016-03-20 16:48:50 -0300

    otimizando carregamento das imagens

--- js/galeria.js
+++ js/galeria.js
@@ -1,8 +1,8 @@
 $(function() {
-    for (var i = 1; i < 55; i++) {
-        var foto = '<div class="grid-item"><a href="img/galeria/foto' + i + '.jpg" data-lightbox="roadtrip"><img src="img/galeria/foto'+i+'.jpg" alt="Foto '+i+'"></a></div>'
-        $('.grid').append(foto);
-    }
+//    for (var i = 1; i < 55; i++) {
+//        var foto = '<div class="grid-item"><a href="img/galeria/foto' + i + '.jpg" data-lightbox="roadtrip"><img src="img/galeria/foto'+i+'.jpg" alt="Foto '+i+'"></a></div>'
+//        $('.grid').append(foto);
+//    }
     $('.grid').masonry({
       // options
       itemSelector: '.grid-item',
