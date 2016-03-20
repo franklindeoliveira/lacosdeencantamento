386fcc435702c2028b5043af5d66ade79507ba12
franklindeoliveira <franklindeoliveira@gmail.com>
2016-03-20 18:47:49 -0300

    otimizando carregamento das imagens 6

--- js/galeria.js
+++ js/galeria.js
@@ -1,6 +1,6 @@
 $(function() {
     for (var i = 1; i < 55; i++) {
-        var foto = '<a href="img/galeria/foto'+i+'.jpg" data-lightbox="roadtrip"><img src="img/galeria/foto'+i+'.jpg" alt="Foto '+i+'"></a>'
+        var foto = '<a href="img/galeria/originais/foto'+i+'.jpg" data-lightbox="roadtrip"><img src="img/galeria/miniaturas/foto'+i+'.jpg" alt="Foto '+i+'"></a>'
         $('.galeria-fotos').append(foto);
     }
 });
