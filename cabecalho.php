24a424d9dbf212ce4e42b232fb0f399d40e5448f
franklindeoliveira <franklindeoliveira@gmail.com>
2016-10-23 22:09:48 -0200

    add .htaccess

--- cabecalho.php
+++ cabecalho.php
@@ -5,20 +5,20 @@
         <ul itemscope itemtype="http://schema.org/BreadcrumbList">
             <?php if (!is_null($inicio)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="index.php">
+                <a itemprop="item" href="index">
                     <button itemprop="name" class="amarelo">Início</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="index.php">
+                <a href="index">
                     <button class="amarelo">Início</button>
                 </a>
             </li>
             <?php } ?>
             <?php if (!is_null($oficinas)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="oficinas.php">
+                <a itemprop="item" href="oficinas">
                     <button itemprop="name" class="laranja">Oficinas e Cursos</button>
                 </a>
             </li>
@@ -31,78 +31,78 @@
             <?php } ?>
             <?php if (!is_null($sobre)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="sobre.php">
+                <a itemprop="item" href="sobre">
                     <button itemprop="name" class="verde">Sobre</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="sobre.php">
+                <a href="sobre">
                     <button class="verde">Sobre</button>
                 </a>
             </li>
             <?php } ?>
             <?php if (!is_null($brincadeiras)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="brincadeiras.php">
+                <a itemprop="item" href="brincadeiras">
                     <button itemprop="name" class="roxo">Brincadeiras</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="brincadeiras.php">
+                <a href="brincadeiras">
                     <button class="roxo">Brincadeiras</button>
                 </a>
             </li>
             <?php } ?>
             <?php if (!is_null($contato)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="contato.php">
+                <a itemprop="item" href="contato">
                     <button itemprop="name" class="laranja">Contato</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="contato.php">
+                <a href="contato">
                     <button class="laranja">Contato</button>
                 </a>
             </li>
             <?php } ?>
             <?php if (!is_null($fotos)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="galeria-fotos.php">
+                <a itemprop="item" href="galeria-fotos">
                     <button itemprop="name" class="amarelo">Galeria de Fotos</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="galeria-fotos.php">
+                <a href="galeria-fotos">
                     <button class="amarelo">Galeria de Fotos</button>
                 </a>
             </li>
             <?php } ?>
             <?php if (!is_null($depoimentos)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="depoimentos.php">
+                <a itemprop="item" href="depoimentos">
                     <button itemprop="name" class="verde">Depoimentos</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="depoimentos.php">
+                <a href="depoimentos">
                     <button class="verde">Depoimentos</button>
                 </a>
             </li>
             <?php } ?>
             <?php if (!is_null($leituras)) { ?>
             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                <a itemprop="item" href="leia.php">
+                <a itemprop="item" href="leituras">
                     <button itemprop="name" class="roxo">Leituras</button>
                 </a>
             </li>
             <?php } else { ?>
             <li>
-                <a href="leituras.php">
+                <a href="leituras">
                     <button class="roxo">Leituras</button>
                 </a>
             </li>
